88ba52794d047c37299fc34d9d14b24091c51823
kwabena <kwabena@mpharma.com>
2021-12-09 01:16:34 +0000

    Add implementation for result base

--- fluent_results/results/resultbase.py
+++ fluent_results/results/resultbase.py
@@ -8,4 +8,18 @@ class ResultBase(ABC):
     """
     Base class for all results
     """
-    pass
+    Errors = []
+
+    Successes = []
+
+    Reasons = []
+
+    data = None
+
+    @property
+    def is_failed(self):
+        return self.Errors.count() > 0
+
+    @property
+    def is_success(self):
+        return not self.is_failed
