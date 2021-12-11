35f87fba8561bdf7aed51f23295a5c1ecea3a669
kwabena <kwabena@mpharma.com>
2021-12-11 09:20:29 +0000

    Add result fail implementation

--- fluent_results/tests/test_results.py
+++ fluent_results/tests/test_results.py
@@ -1,10 +1,35 @@
+import pytest
 """
 Test case for results.py
 """
 from results.result import Result
 
 def test_result_ok_with_valid_data():
-    result = Result.Ok("Tested successfully")
+    Result.ok("Successful One")
+    result = Result.ok({"data": "sample data"}, "Tested successfully")
 
-    assert len(result.Successes) == 1
-    assert result.data == None
+    assert len(result.successes) == 1
+    assert len(result.errors) == 0
+    assert len(result.reasons) == 0
+    assert result.successes[0] == "Tested successfully"
+    assert result.value == {"data": "sample data"}
+    assert result.is_success
+    assert not result.is_failed
+
+def test_result_ok_with_invalid_data():
+    with pytest.raises(TypeError):
+        Result.ok(["Somthing"], 99.8)
+
+def test_result_fail_with_valid_data():
+    Result.fail("Failure")
+    result = Result.fail("Process failed")
+
+    assert len(result.successes) == 0
+    assert len(result.errors) == 1
+    assert len(result.reasons) == 0
+    assert result.errors[0] == "Process failed"
+    assert result.value == None
+    assert not result.is_success
+    assert result.is_failed
+
+
