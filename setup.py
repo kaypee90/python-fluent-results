908a2e198e05172dc959b6d75e1de565f5fb474b
kaypee90 <kaypee90@yahoo.com>
2021-12-28 22:29:04 +0000

    Update setup.py file

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="Python Fluent Results",
     url="https://github.com/kaypee90/python-fluent-results",
     author="kaypee90",
     author_email="kaypee90@yahoo.com",
-    packages=["fluent_results"],
     install_requires=["schema==0.7.5"],
     version="0.0.1",
     license="MIT",
     description="Propagate errors and data nicely with a uniform interface in your python project",
-    long_description=open("README.md").read(),
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "fluent_results"},
+    packages=setuptools.find_packages(where="fluent_results"),
     python_requires=">=3.6",
 )
